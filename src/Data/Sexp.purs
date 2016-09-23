26847246493b2c36e10e87cf368a27054fe4ecbc
rightfold <rightfold@gmail.com>
2016-09-23 19:07:54 +0200

    Add FromSexp instance for GenericSpine

--- src/Data/Sexp.purs
+++ src/Data/Sexp.purs
@@ -4,12 +4,12 @@ module Data.Sexp
 , toString
 , class ToSexp, toSexp
 , class FromSexp, fromSexp
-, gToSexp
+, gToSexp, gFromSexp
 ) where
 
 import Data.Either (Either(..))
 import Data.Foldable (foldMap)
-import Data.Generic (class Generic, GenericSpine(..), toSpine)
+import Data.Generic (fromSpine, class Generic, GenericSpine(..), toSpine)
 import Data.Int as Int
 import Data.List ((:), List(..))
 import Data.List as List
@@ -50,6 +50,9 @@ instance toSexpUnit :: ToSexp Unit where
 instance toSexpBoolean :: ToSexp Boolean where
   toSexp true  = Atom "true"
   toSexp false = Atom "false"
+
+instance toSexpChar :: ToSexp Char where
+  toSexp c = Atom (String.singleton c)
 
 instance toSexpInt :: ToSexp Int where
   toSexp n = Atom (show n)
@@ -92,13 +95,13 @@ instance toSexpStrMap :: (ToSexp v) => ToSexp (StrMap v) where
   toSexp xs = List (StrMap.toList xs >>= \(Tuple k v) -> Atom k : toSexp v : Nil)
 
 instance toSexpGenericSpine :: ToSexp GenericSpine where
-  toSexp SUnit        = List (Atom "SUnit"    : Nil)
+  toSexp SUnit        = Atom "SUnit"
   toSexp (SArray xs)  = List (Atom "SArray"   : List.fromFoldable (map (\thk -> toSexp (thk unit)) xs))
-  toSexp (SChar c)    = List (Atom "SChar"    : Atom (String.singleton c) : Nil)
-  toSexp (SString s)  = List (Atom "SString"  : Atom s : Nil)
-  toSexp (SInt i)     = List (Atom "SInt"     : Atom (show i) : Nil)
-  toSexp (SBoolean b) = List (Atom "SBoolean" : Atom (show b) : Nil)
-  toSexp (SNumber n)  = List (Atom "SNumber"  : Atom (show n) : Nil)
+  toSexp (SChar c)    = List (Atom "SChar"    : toSexp c : Nil)
+  toSexp (SString s)  = List (Atom "SString"  : toSexp s : Nil)
+  toSexp (SInt i)     = List (Atom "SInt"     : toSexp i : Nil)
+  toSexp (SBoolean b) = List (Atom "SBoolean" : toSexp b : Nil)
+  toSexp (SNumber n)  = List (Atom "SNumber"  : toSexp n : Nil)
   toSexp (SRecord r)  = List (Atom "SRecord"  : List.fromFoldable (r >>= \{recLabel, recValue} -> [Atom recLabel, toSexp (recValue unit)]))
   toSexp (SProd c xs) = List (Atom "SProd"    : Atom c : List.fromFoldable (map (\thk -> toSexp (thk unit)) xs))
 
@@ -116,6 +119,10 @@ instance fromSexpUnit :: FromSexp Unit where
 instance fromSexpBoolean :: FromSexp Boolean where
   fromSexp (Atom "true")  = Just true
   fromSexp (Atom "false") = Just false
+  fromSexp _ = Nothing
+
+instance fromSexpChar :: FromSexp Char where
+  fromSexp (Atom x) = String.toChar x
   fromSexp _ = Nothing
 
 instance fromSexpInt :: FromSexp Int where
@@ -154,6 +161,30 @@ instance fromSexpEither :: (FromSexp a, FromSexp b) => FromSexp (Either a b) whe
   fromSexp (List (Atom "Right" : x : Nil)) = Right <$> fromSexp x
   fromSexp _ = Nothing
 
+instance fromSexpGenericSpine :: FromSexp GenericSpine where
+  fromSexp       (Atom "SUnit")                  = Just SUnit
+  fromSexp (List (Atom "SArray"   : xs))         =
+    SArray <<< List.toUnfoldable <<< map const <$> traverse fromSexp xs
+  fromSexp (List (Atom "SChar"    : c : Nil))    = SChar <$> fromSexp c
+  fromSexp (List (Atom "SString"  : s : Nil))    = fromSexp s
+  fromSexp (List (Atom "SInt"     : i : Nil))    = fromSexp i
+  fromSexp (List (Atom "SBoolean" : b : Nil))    = fromSexp b
+  fromSexp (List (Atom "SNumber"  : n : Nil))    = fromSexp n
+  fromSexp (List (Atom "SRecord"  : r))          =
+    let go acc Nil = Just acc
+        go acc (Atom k : v : tail) = do
+          v' <- fromSexp v
+          go ({recLabel: k, recValue: \_ -> v'} : acc) tail
+        go acc _ = Nothing
+    in SRecord <<< List.toUnfoldable <$> go Nil r
+  fromSexp (List (Atom "SProd"    : Atom c : r)) =
+    SProd c <<< List.toUnfoldable <<< map const <$> traverse fromSexp r
+  fromSexp _ = Nothing
+
 -- | Convert anything to an S-expression.
 gToSexp :: forall a. (Generic a) => a -> Sexp
 gToSexp = toSpine >>> toSexp
+
+-- | Convert an S-expression to anything.
+gFromSexp :: forall a. (Generic a) => Sexp -> Maybe a
+gFromSexp = fromSexp >=> fromSpine
